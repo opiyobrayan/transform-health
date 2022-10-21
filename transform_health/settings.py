ac654b505ac9bb4ad06e2c0ceadaa33118615b7a
opiyobrayan <67815971+opiyobrayan@users.noreply.github.com>
2022-10-21 13:35:01 +0300

    Update settings.py

--- transform_health/settings.py
+++ transform_health/settings.py
@@ -118,6 +118,7 @@ STATICFILES_DIR=[
 ]
 MEDIA_URL = "/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"static/media/")
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
