adf5d6257ec2f0442ce4385859b8c1d0519c9adf
opiyobrayan <67815971+opiyobrayan@users.noreply.github.com>
2022-10-21 12:12:58 +0300

    Update settings.py

--- transform_health/settings.py
+++ transform_health/settings.py
@@ -123,3 +123,5 @@ MEDIA_ROOT=os.path.join(BASE_DIR,"static/media/")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS=['https://*thc-kenya.up.railway.app/']
